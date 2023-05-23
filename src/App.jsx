223ad45c1e6fc71000eb48580135e008b8a9289d
lukasz-wasyleczko <lukasz@activate.co.uk>
2023-05-23 12:49:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+const POSTS = [
+    {}
+]
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+        HEY
+    </>
+  )
+}
+
+function wait(duration){
+    return new Promise(resolve => setTimeout(resolve,duration))
+}
+
+export default App
